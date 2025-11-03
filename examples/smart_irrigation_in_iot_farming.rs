702b086fdf38af41b8f48f87a199d8534c63aadd
Marcin Bielak <marcin.bieli@gmail.com>
2025-11-03 23:49:42 +0100

    fix CI with cargo fmt

--- examples/smart_irrigation_in_iot_farming.rs
+++ examples/smart_irrigation_in_iot_farming.rs
@@ -1,27 +1,51 @@
 use fuzzy_logic_engine_rs::{
     membership::MembershipKind as M,
+    rule::{Connective, Rule},
     term::Term,
     variable::{LinguisticVariable, Range},
-    rule::{Rule, Connective},
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // TODO: add system definition
 
     // Output: irrigation level (0 = off, 100 = max flow)
-    let mut irrigation = LinguisticVariable::new("irrigation", Range { min: 0.0, max: 100.0 });
+    let mut irrigation = LinguisticVariable::new(
+        "irrigation",
+        Range {
+            min: 0.0,
+            max: 100.0,
+        },
+    );
     // TODO: add output definition
 
     // Input: soil moisture (% volumetric water content)
-    let mut soil = LinguisticVariable::new("soil_moisture", Range { min: 0.0, max: 100.0 });
+    let mut soil = LinguisticVariable::new(
+        "soil_moisture",
+        Range {
+            min: 0.0,
+            max: 100.0,
+        },
+    );
     // TODO: add input definitions
 
     // Input: rain forecast (mm expected in next 24h)
-    let mut rain = LinguisticVariable::new("rain_forecast", Range { min: 0.0, max: 50.0 });
+    let mut rain = LinguisticVariable::new(
+        "rain_forecast",
+        Range {
+            min: 0.0,
+            max: 50.0,
+        },
+    );
     // TODO: add input definitions
 
     // Input: crop growth stage (0 = germination, 10 = harvest)
-    let mut stage = LinguisticVariable::new("growth_stage", Range { min: 0.0, max: 10.0 });
+    let mut stage = LinguisticVariable::new(
+        "growth_stage",
+        Range {
+            min: 0.0,
+            max: 10.0,
+        },
+    );
     // TODO: add input definitions
 
     // TODO: Prepare farming dedicated rules:
@@ -33,6 +57,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // - Heavy rain -> off (save water, avoid runoff)
 
     // TODO: add example and inference for IoT readings
-  
+
     Ok(())
 }
