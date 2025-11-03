c10ed70c8974570a32453e83032072ab0cd86db6
Marcin Bielak <marcin.bieli@gmail.com>
2025-11-03 23:39:43 +0100

    fix CI with cargo fmt

--- examples/robotic_behaviours__abstacle_avoidance.rs
+++ examples/robotic_behaviours__abstacle_avoidance.rs
@@ -1,23 +1,41 @@
 use fuzzy_logic_engine_rs::{
     membership::MembershipKind as M,
+    rule::{Connective, Rule},
     term::Term,
     variable::{LinguisticVariable, Range},
-    rule::{Rule, Connective},
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // TODO: add system definition
 
     // Output: speed
-    let mut speed = LinguisticVariable::new("speed", Range { min: 0.0, max: 100.0 });
+    let mut speed = LinguisticVariable::new(
+        "speed",
+        Range {
+            min: 0.0,
+            max: 100.0,
+        },
+    );
     // TODO: add output definition
 
     // Input: distance to obstacle
-    let mut distance = LinguisticVariable::new("distance", Range { min: 0.0, max: 200.0 });
+    let mut distance = LinguisticVariable::new(
+        "distance",
+        Range {
+            min: 0.0,
+            max: 200.0,
+        },
+    );
     // TODO: add input definitions
 
     // Input: battery level
-    let mut battery = LinguisticVariable::new("battery", Range { min: 0.0, max: 100.0 });
+    let mut battery = LinguisticVariable::new(
+        "battery",
+        Range {
+            min: 0.0,
+            max: 100.0,
+        },
+    );
     // TODO: add input definitions
 
     // TODO: add rules
