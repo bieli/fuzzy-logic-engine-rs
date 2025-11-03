fb242281866bf3c95f77a56e40185de290687f9e
Marcin Bielak <marcin.bieli@gmail.com>
2025-11-03 22:26:44 +0100

    Add degree method to MembershipKind for Triangle
    Implement degree function for Triangle membership.

--- src/membership.rs
+++ src/membership.rs
@@ -4,3 +4,16 @@ pub enum MembershipKind {
     Trapezoid { a: f64, b: f64, c: f64, d: f64 },
     Gauss { sigma: f64, mu: f64 },
 }
+
+impl MembershipKind {
+    pub fn degree(&self, x: f64) -> f64 {
+        match *self {
+            MembershipKind::Triangle { a, b, c } => {
+                if x <= a || x >= c { 0.0 }
+                else if x == b { 1.0 }
+                else if x < b { (x - a) / (b - a) }
+                else { (c - x) / (c - b) }
+            }
+        }
+    }
+}
