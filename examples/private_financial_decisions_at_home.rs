e4acc0d6f460d954222b7e10c48ee4122698390a
Marcin Bielak <marcin.bieli@gmail.com>
2025-11-03 23:13:31 +0100

    Add fuzzy logic framework for financial decisions
    Initial implementation of fuzzy logic variables for financial decisions.

---
+++ examples/private_financial_decisions_at_home.rs
@@ -0,0 +1,29 @@
+use fuzzy_logic_engine_rs::{
+    membership::MembershipKind as M,
+    term::Term,
+    variable::{LinguisticVariable, Range},
+    rule::{Rule, Connective},
+};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // TODO: add system definition
+
+    // Output: savings rate (% of income)
+    let mut savings = LinguisticVariable::new("savings", Range { min: 0.0, max: 50.0 });
+    // TODO: add output definitions
+
+    // Input: income stability (0 = unstable, 10 = very stable)
+    let mut stability = LinguisticVariable::new("stability", Range { min: 0.0, max: 10.0 });
+    // TODO: add input definitions
+  
+    // Input: current expenses (% of income)
+    let mut expenses = LinguisticVariable::new("expenses", Range { min: 0.0, max: 100.0 });
+    // TODO: add input definitions
+
+    // Rules
+    // TODO: add rules
+
+    // TODO: process & show results
+  
+    Ok(())
+}
