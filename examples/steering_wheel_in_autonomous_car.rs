51ffdf3f602e775f5d5044e272ec93ce4c42784b
Marcin Bielak <marcin.bieli@gmail.com>
2025-11-04 20:29:18 +0100

    update examples

--- examples/steering_wheel_in_autonomous_car.rs
+++ examples/steering_wheel_in_autonomous_car.rs
@@ -1,4 +1,5 @@
 use fuzzy_logic_engine_rs::{
+    fis::{FisType, FuzzyInferenceSystem},
     membership::MembershipKind as M,
     rule::{Connective, Rule},
     term::Term,
@@ -6,7 +7,7 @@ use fuzzy_logic_engine_rs::{
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // TODO: add system definition
+    let mut system = FuzzyInferenceSystem::new("Autonomous steering by fuzzy logic");
 
     // Output: steering angle (negative = left, positive = right)
     let mut steering = LinguisticVariable::new(
@@ -16,7 +17,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             max: 30.0,
         },
     );
-    // TODO: add output definitions
+    steering.add_term(Term::new(
+        "left",
+        M::Triangle {
+            a: -30.0,
+            b: -20.0,
+            c: -10.0,
+        },
+    ));
+    steering.add_term(Term::new(
+        "straight",
+        M::Triangle {
+            a: -5.0,
+            b: 0.0,
+            c: 5.0,
+        },
+    ));
+    steering.add_term(Term::new(
+        "right",
+        M::Triangle {
+            a: 10.0,
+            b: 20.0,
+            c: 30.0,
+        },
+    ));
+    system.add_output(steering);
 
     // Input: lane deviation (meters from center)
     let mut deviation = LinguisticVariable::new(
@@ -26,7 +51,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             max: 2.0,
         },
     );
-    // TODO: add input definitions
+    deviation.add_term(Term::new(
+        "left",
+        M::Triangle {
+            a: -2.0,
+            b: -2.0,
+            c: -0.5,
+        },
+    ));
+    deviation.add_term(Term::new(
+        "center",
+        M::Triangle {
+            a: -0.5,
+            b: 0.0,
+            c: 0.5,
+        },
+    ));
+    deviation.add_term(Term::new(
+        "right",
+        M::Triangle {
+            a: 0.5,
+            b: 2.0,
+            c: 2.0,
+        },
+    ));
+    system.add_input(deviation);
 
     // Input: road curvature (negative = left curve, positive = right curve)
     let mut curvature = LinguisticVariable::new(
@@ -36,10 +85,60 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             max: 1.0,
         },
     );
-    // TODO: add input definitions
+    curvature.add_term(Term::new(
+        "left",
+        M::Triangle {
+            a: -1.0,
+            b: -1.0,
+            c: -0.3,
+        },
+    ));
+    curvature.add_term(Term::new(
+        "straight",
+        M::Triangle {
+            a: -0.2,
+            b: 0.0,
+            c: 0.2,
+        },
+    ));
+    curvature.add_term(Term::new(
+        "right",
+        M::Triangle {
+            a: 0.3,
+            b: 1.0,
+            c: 1.0,
+        },
+    ));
+    system.add_input(curvature);
 
-    // TODO: add rules
+    // Rules
+    system.set_rules(vec![
+        Rule::new(
+            vec![Some("left".into()), Some("straight".into())],
+            vec!["right".into()],
+            Connective::And,
+        ),
+        Rule::new(
+            vec![Some("right".into()), Some("straight".into())],
+            vec!["left".into()],
+            Connective::And,
+        ),
+        Rule::new(
+            vec![Some("center".into()), Some("left".into())],
+            vec!["left".into()],
+            Connective::And,
+        ),
+        Rule::new(
+            vec![Some("center".into()), Some("right".into())],
+            vec!["right".into()],
+            Connective::And,
+        ),
+    ]);
 
-    // TODO: add applying rules and show results
+    let result = system.compute(FisType::Mamdani, &[-0.8, 0.0])?;
+    println!("Steering decision: {:?}", result);
+    assert!(result[0] > 19.0);
+    assert!(result[0] < 20.0);
+
     Ok(())
 }
